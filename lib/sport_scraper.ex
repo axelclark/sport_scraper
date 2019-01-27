1867659f9d60c23c894dfd789e657aad0b4a17d4
Axel Clark <axelclark2@yahoo.com>
2019-01-27 09:19:14 -0500

    Remove default functions from SportScraper

--- lib/sport_scraper.ex
+++ lib/sport_scraper.ex
@@ -1,18 +1,2 @@
 defmodule SportScraper do
-  @moduledoc """
-  Documentation for SportScraper.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SportScraper.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
